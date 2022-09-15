72765b69b3cf8d7e94d88a74372803e743632dab
ManishA0799 <manishatyalkar29@gmail.com>
2022-09-15 14:51:20 +0530

    Add project files.

---
+++ EMP Salary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_15_9_2022
+{
+    public class EmpSalary
+    {
+        private int empid;
+        private string empname;
+        private int basicsalary;
+        public int total;
+        public int netsalary;
+        public int hra, da, pt, pf;
+
+
+        public EmpSalary(int empid, string empname, int basicsalary)
+        {
+            this.empid = empid;
+            this.empname = empname;
+            this.basicsalary = basicsalary;
+        }
+
+        public void Calculate()
+        {
+            da = (basicsalary * 20) / 100;
+            hra = (basicsalary* 40) / 100;
+            pf = (basicsalary * 12) / 100;
+            pt = (basicsalary - 200);
+
+            total = basicsalary + hra + da;
+            netsalary = (total - pt - pf);     
+                        
+        }
+
+        public string DisplayValue()
+        {
+            return $" Id ==>{empid} \n Name : {empname} \n Basic Salary : {basicsalary} \n  Total Salary: {total} \n Net Salary : {netsalary}";
+        }
+
+
+
+
+
+
+
+
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_15_9_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a class Emp with empid, empname, accept bs from constructor, & calculate salary as follows
+ 
+             HRA -> 40 % of BS
+             DA  -> 20 % of BS
+             PT -> 200
+             PF  -> 12 % of BS
+
+
+             Calculate the total salary & net paid salary
+             total=bs + allows  
+             Net paid = total-pf -pt
+            *
+            */
+
+            EmpSalary empSalary = new EmpSalary(021, "Manish", 25000);
+            empSalary.Calculate();
+            Console.WriteLine(empSalary.DisplayValue());
+
+        }
+    }
+}
