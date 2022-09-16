82c467824f6c57d598d3abc581ccce20b1e0184a
ManishA0799 <manishatyalkar29@gmail.com>
2022-09-16 10:23:56 +0530

    get set

---
+++ ger set.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_15_9_2022
+{
+    public class Student
+    {
+        private int rollno;
+        private string name;
+        private double per;
+
+        public int Rollno
+        {
+            get { return rollno; }
+            set { rollno = value; } 
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Percentage
+        {
+            get { return per; }
+            set { per = value; }
+
+        //    public override string ToString()
+        //{
+        //    return $"Roll -> {rollno} Name {name}  percentage ->{per} ";
+
+        //}
+    }
+    }
+
+
+
+
+
+
+
+
+
+}
